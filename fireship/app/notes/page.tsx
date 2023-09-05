ac6080f68d2c792451c3239b71e043992dc79687
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-05 16:47:11 -0700

    setting up dummy fetch and data handling

--- fireship/app/notes/page.tsx
+++ fireship/app/notes/page.tsx
@@ -1,8 +1,12 @@
 async function getNotes() {
-    const res = await fetch()
+    const res = await fetch('https://mydbwebsite.com/api/notes');
+    const data = await res.json();
+    return data?.items as any[];
 }
 
 export default async function NotesPage() {
+    const notes = await getNotes();
+    
     return(
         <div>
 
