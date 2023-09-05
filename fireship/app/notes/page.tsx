bbe08a7760f79644edb2d30d78d38f3fa6f17569
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-05 16:58:19 -0700

    Created note component

--- fireship/app/notes/page.tsx
+++ fireship/app/notes/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 async function getNotes() {
     const res = await fetch('https://mydbwebsite.com/api/notes');
     const data = await res.json();
@@ -6,10 +7,29 @@ async function getNotes() {
 
 export default async function NotesPage() {
     const notes = await getNotes();
-    
+
     return(
         <div>
-
+            <h1>Notes</h1>
+            <div>
+                {notes?.map((note) => {
+                    return <Note key={note.id} note={note}/>
+                })}
+            </div>
         </div>
+    )
+}
+
+function Note({ note }: any): any {
+    const { id, title, content, created } = note || {};
+
+    return(
+        <Link href={`/notes/${id}`}>
+            <div>
+                <h2>{title}</h2>
+                <h5>{content}</h5>
+                <p>{created}</p>
+            </div>
+        </Link>
     )
 }
