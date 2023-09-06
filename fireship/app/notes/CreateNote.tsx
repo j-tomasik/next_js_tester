1cbc26c73ce0a4edec0347cf7c5e1040d9c01dd8
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-05 17:41:08 -0700

    Creating form input and state

--- fireship/app/notes/CreateNote.tsx
+++ fireship/app/notes/CreateNote.tsx
@@ -2,11 +2,28 @@
 
 import { useState } from "react";
 
+
 export default function CreateNote() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
 
     return(
         <form>
-            
+            <h3>Create a new Note</h3>
+            <input
+                type='text'
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea 
+                placeholder="Content"
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+            />
+            <button type="submit">
+                Create Note
+            </button>
         </form>
     )
 }
