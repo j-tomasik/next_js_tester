2f92f6b89f3bd546240d9e8ab37e4dcc49c157f2
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-07 15:35:29 -0700

    Created buttons for new note form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 
 export default function Page() {
@@ -5,8 +6,17 @@ export default function Page() {
         <header className="flex justify-between items-center mb-4">
             <h1 className="text-2xl">New</h1>
         </header>
-        <form>
+        <form className="flex gap-2 flex-col">
             <input type='text' name='title' className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:boarder-slate-100"/>
+            <div className='flex gap-1 justify-end'>
+                <Link href='..' 
+                    className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>
+                    Cancel</Link>
+                <button 
+                type='submit' 
+                className='border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none'>
+                    Create</button>
+            </div>
         </form>
     </>
 }
