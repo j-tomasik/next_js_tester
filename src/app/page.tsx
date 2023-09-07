08d13aba3241e0b52216e0db52072a1bb060fa6e
Jack Tomasik <jackedwardtomasik@gmail.com>
2023-09-07 15:11:45 -0700

    fetching todos with next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import prisma  from '@/db';
 
-export default function Home() {
+export default async function Home() {
+  const todos = await prisma.todo.findMany()
+
   return <>
     <header className="flex justify-between items-center mb-4">
       <h1 className="text-2xl">Todos</h1>
-      <Link href='/new'>New</Link>
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href='/new'>New</Link>
     </header>
-    <ul>
-
+    <ul className='pl-4'>
+      {todos.map(todo =>  (
+        <li>{todo.title}</li>
+      ))}
     </ul>
   </>
 }
